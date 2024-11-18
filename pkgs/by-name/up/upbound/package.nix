f6f4c1d611d9e652f119ca84f6b456bcf5fa9a45
Jean-Luc Jox <jeanluc.jox@gmail.com>
2024-11-18 14:06:12 +1000

    upbound: 0.34.0 -> 0.35.3

--- pkgs/by-name/up/upbound/package.nix
+++ pkgs/by-name/up/upbound/package.nix
@@ -1,32 +1,63 @@
-{ lib, buildGoModule, fetchFromGitHub }:
-
-buildGoModule rec {
+{
+  system ? builtins.currentSystem,
+  lib,
+  fetchurl,
+  installShellFiles,
+  stdenvNoCC,
+}:
+let
+  sources = import ./sources.nix;
+  version = sources.version;
+  arch = sources.archMap.${system};
+  fetchurlAttr = sources.fetchurlAttrSet.${system};
+in
+stdenvNoCC.mkDerivation {
   pname = "upbound";
-  version = "0.34.0";
+  inherit version;
+  srcs = [
+    (fetchurl {
+      url = fetchurlAttr.docker-credential-up.url;
+      sha256 = fetchurlAttr.docker-credential-up.hash;
+    })
 
-  src = fetchFromGitHub {
-    owner = pname;
-    repo = "up";
-    rev = "v${version}";
-    hash = "sha256-npWCGR0/iXAF7efreWhpwqboXJ24lqk5TPaB7glwEFI=";
-  };
-
-  vendorHash = "sha256-f/RPyjrC5FsOjC5wnBtqEbLeVX7VeiVYFpRXcQ2lTc4=";
-
-  subPackages = [ "cmd/docker-credential-up" "cmd/up" ];
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/upbound/up/internal/version.version=v${version}"
+    (fetchurl {
+      url = fetchurlAttr.up.url;
+      sha256 = fetchurlAttr.up.hash;
+    })
   ];
 
-  meta = with lib; {
-    description =
-      "CLI for interacting with Upbound Cloud, Upbound Enterprise, and Universal Crossplane (UXP)";
+  sourceRoot = ".";
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+
+    cp ./${arch}/up $out/bin/up
+    chmod +x $out/bin/up
+
+    cp ./${arch}/docker-credential-up $out/bin/docker-credential-up
+    chmod +x $out/bin/docker-credential-up
+
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    installShellCompletion --bash --name up <(echo complete -C up up)
+  '';
+
+  system = system;
+
+  passthru.updateScript = ./update.sh;
+
+  meta = {
+    description = "CLI for interacting with Upbound Cloud, Upbound Enterprise, and Universal Crossplane (UXP)";
     homepage = "https://upbound.io";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ lucperkins ];
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ lucperkins ];
     mainProgram = "up";
+    platforms = sources.platformList;
   };
 }
